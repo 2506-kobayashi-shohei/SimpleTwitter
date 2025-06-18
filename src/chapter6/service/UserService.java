3ca782bdb710eb416c4cb81a3d19aad56638a745
kobayashi_shohei <kobayashi.shohei@alhinc.jp>
2025-06-18 15:23:14 +0900

    １回目打鍵テスト後の修正＆実践問題①　設定画面でパスワードを入力しなかった場合、その他の項目のみ更新されるようにする。

--- src/chapter6/service/UserService.java
+++ src/chapter6/service/UserService.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.dao.UserDao;
 import chapter6.logging.InitApplication;
@@ -118,9 +120,10 @@ public class UserService {
         Connection connection = null;
         try {
             // パスワード暗号化
-            String encPassword = CipherUtil.encrypt(user.getPassword());
-            user.setPassword(encPassword);
-
+        	if(!StringUtils.isEmpty(user.getPassword())) {
+                String encPassword = CipherUtil.encrypt(user.getPassword());
+                user.setPassword(encPassword);
+        	}
             connection = getConnection();
             new UserDao().update(connection, user);
             commit(connection);
